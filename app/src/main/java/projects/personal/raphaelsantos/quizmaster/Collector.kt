6884934645b4e3edf8ca55d355a1f2b89c375c6f
raphaelsantos <raphaelmcsantos.santos@gmai.com>
2018-03-17 19:51:37 -0300

    Getting started

---
+++ app/src/main/java/projects/personal/raphaelsantos/quizmaster/Collector.kt
@@ -0,0 +1,12 @@
+package projects.personal.raphaelsantos.quizmaster
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Collector : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collector)
+    }
+}
